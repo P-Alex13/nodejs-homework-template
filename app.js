256f379dd34ae5e7e4a60f7dbddea3f73b0615e5
Iulia Popoaca <109438791+P-Alex13@users.noreply.github.com>
2024-06-23 21:50:19 -0700

    done hw04

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ import express from "express";
 import logger from "morgan";
 import cors from "cors";
 import contactsRouter from "./routes/api/contacts.js";
-import connectToDb  from "./utils/connectToDb.js";
+import usersRouter from "./routes/api/users.js";
+import connectToDb from "./utils/connectToDb.js";
+
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
@@ -14,6 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
+app.use("/api/users", usersRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
